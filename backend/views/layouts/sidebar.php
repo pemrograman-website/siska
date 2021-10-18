294141086e95217d99666acf01629eeb14c04257
Yusuf Yudhistira <yudhis96@yahoo.com>
2021-10-19 06:24:35 +0700

    Tweaking Sidebar

--- backend/views/layouts/sidebar.php
+++ backend/views/layouts/sidebar.php
@@ -66,32 +66,32 @@
 
 
                     ['label' => 'DOSEN', 'header' => true],
-                    ['label' => 'Ubah Profil', 'url' => ['site/login'], 'icon' => 'sign-in-alt', 'visible' => Yii::$app->user->isGuest],
+                    ['label' => 'Ubah Profil', 'url' => ['site/login'], 'icon' => 'sign-in-alt'],
+                    ['label' => 'Lihat Kelas', 'url' => ['site/login'], 'icon' => 'sign-in-alt'],
 
-                    ['label' => 'Debug', 'icon' => 'bug', 'url' => ['/debug'], 'target' => '_blank'],
-                    ['label' => 'MULTI LEVEL EXAMPLE', 'header' => true],
-                    ['label' => 'Level1'],
-                    [
-                        'label' => 'Level1',
-                        'items' => [
-                            ['label' => 'Level2', 'iconStyle' => 'far'],
-                            [
-                                'label' => 'Level2',
-                                'iconStyle' => 'far',
-                                'items' => [
-                                    ['label' => 'Level3', 'iconStyle' => 'far', 'icon' => 'dot-circle'],
-                                    ['label' => 'Level3', 'iconStyle' => 'far', 'icon' => 'dot-circle'],
-                                    ['label' => 'Level3', 'iconStyle' => 'far', 'icon' => 'dot-circle']
-                                ]
-                            ],
-                            ['label' => 'Level2', 'iconStyle' => 'far']
-                        ]
-                    ],
-                    ['label' => 'Level1'],
-                    ['label' => 'LABELS', 'header' => true],
-                    ['label' => 'Important', 'iconStyle' => 'far', 'iconClassAdded' => 'text-danger'],
-                    ['label' => 'Warning', 'iconClass' => 'nav-icon far fa-circle text-warning'],
-                    ['label' => 'Informational', 'iconStyle' => 'far', 'iconClassAdded' => 'text-info'],
+                    // ['label' => 'MULTI LEVEL EXAMPLE', 'header' => true],
+                    // ['label' => 'Level1'],
+                    // [
+                    //     'label' => 'Level1',
+                    //     'items' => [
+                    //         ['label' => 'Level2', 'iconStyle' => 'far'],
+                    //         [
+                    //             'label' => 'Level2',
+                    //             'iconStyle' => 'far',
+                    //             'items' => [
+                    //                 ['label' => 'Level3', 'iconStyle' => 'far', 'icon' => 'dot-circle'],
+                    //                 ['label' => 'Level3', 'iconStyle' => 'far', 'icon' => 'dot-circle'],
+                    //                 ['label' => 'Level3', 'iconStyle' => 'far', 'icon' => 'dot-circle']
+                    //             ]
+                    //         ],
+                    //         ['label' => 'Level2', 'iconStyle' => 'far']
+                    //     ]
+                    // ],
+                    // ['label' => 'Level1'],
+                    // ['label' => 'LABELS', 'header' => true],
+                    // ['label' => 'Important', 'iconStyle' => 'far', 'iconClassAdded' => 'text-danger'],
+                    // ['label' => 'Warning', 'iconClass' => 'nav-icon far fa-circle text-warning'],
+                    // ['label' => 'Informational', 'iconStyle' => 'far', 'iconClassAdded' => 'text-info'],
                 ],
             ]);
             ?>
