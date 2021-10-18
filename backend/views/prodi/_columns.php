9f1161da82f6c9c8f754bcbb54e5624ffdfbd798
Yusuf Yudhistira <yudhis96@yahoo.com>
2021-10-18 14:24:25 +0700

    Menghilangkan kolom prodi_id pada gridview.

--- backend/views/prodi/_columns.php
+++ backend/views/prodi/_columns.php
@@ -1,4 +1,5 @@
 <?php
+
 use yii\helpers\Url;
 
 return [
@@ -10,21 +11,17 @@ return [
         'class' => 'kartik\grid\SerialColumn',
         'width' => '30px',
     ],
-        [
-        'class'=>'\kartik\grid\DataColumn',
-        'attribute'=>'prodi_id',
+    [
+        'class' => '\kartik\grid\DataColumn',
+        'attribute' => 'kode',
     ],
     [
-        'class'=>'\kartik\grid\DataColumn',
-        'attribute'=>'kode',
+        'class' => '\kartik\grid\DataColumn',
+        'attribute' => 'nama',
     ],
     [
-        'class'=>'\kartik\grid\DataColumn',
-        'attribute'=>'nama',
-    ],
-    [
-        'class'=>'\kartik\grid\DataColumn',
-        'attribute'=>'fakultas_id',
+        'class' => '\kartik\grid\DataColumn',
+        'attribute' => 'fakultas_id',
     ],
     [
         'class' => 'kartik\grid\ActionColumn',
@@ -32,18 +29,20 @@ return [
         'noWrap' => 'true',
         'template' => '{view} {update} {delete}',
         'vAlign' => 'middle',
-        'urlCreator' => function($action, $model, $key, $index) { 
-                return Url::to([$action,'id'=>$key]);
+        'urlCreator' => function ($action, $model, $key, $index) {
+            return Url::to([$action, 'id' => $key]);
         },
         'viewOptions' => ['role' => 'modal-remote', 'title' => Yii::t('yii2-ajaxcrud', 'View'), 'data-toggle' => 'tooltip', 'class' => 'btn btn-sm btn-outline-success'],
         'updateOptions' => ['role' => 'modal-remote', 'title' => Yii::t('yii2-ajaxcrud', 'Update'), 'data-toggle' => 'tooltip', 'class' => 'btn btn-sm btn-outline-primary'],
-        'deleteOptions' => ['role' => 'modal-remote', 'title' => Yii::t('yii2-ajaxcrud', 'Delete'), 'class' => 'btn btn-sm btn-outline-danger', 
+        'deleteOptions' => [
+            'role' => 'modal-remote', 'title' => Yii::t('yii2-ajaxcrud', 'Delete'), 'class' => 'btn btn-sm btn-outline-danger',
             'data-confirm' => false,
-            'data-method' => false,// for overide yii data api
+            'data-method' => false, // for overide yii data api
             'data-request-method' => 'post',
             'data-toggle' => 'tooltip',
             'data-confirm-title' => Yii::t('yii2-ajaxcrud', 'Delete'),
-            'data-confirm-message' => Yii::t('yii2-ajaxcrud', 'Delete Confirm') ],
+            'data-confirm-message' => Yii::t('yii2-ajaxcrud', 'Delete Confirm')
+        ],
     ],
 
-];   
+];
