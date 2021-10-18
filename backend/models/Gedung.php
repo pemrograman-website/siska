f4cb3b245073a967e8a46c7a4faea6b7599313e8
Yusuf Yudhistira <yudhis96@yahoo.com>
2021-10-18 13:52:54 +0700

    Membuat model Gedung.

---
+++ backend/models/Gedung.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "gedung".
+ *
+ * @property int $id
+ * @property string|null $kode
+ * @property string|null $nama
+ */
+class Gedung extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'gedung';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['kode'], 'string', 'max' => 10],
+            [['nama'], 'string', 'max' => 45],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'kode' => 'Kode',
+            'nama' => 'Nama',
+        ];
+    }
+}
