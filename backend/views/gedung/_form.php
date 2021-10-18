436cff07a8f97e09a0ac838c2afbd0d9d46d0bc2
Yusuf Yudhistira <yudhis96@yahoo.com>
2021-10-18 14:04:36 +0700

    CRUD model Gedung

---
+++ backend/views/gedung/_form.php
@@ -0,0 +1,27 @@
+<?php
+use yii\helpers\Html;
+use yii\bootstrap4\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\Gedung */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="gedung-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'kode')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'nama')->textInput(['maxlength' => true]) ?>
+
+  
+	<?php if (!Yii::$app->request->isAjax){ ?>
+	  	<div class="form-group">
+	        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+	    </div>
+	<?php } ?>
+
+    <?php ActiveForm::end(); ?>
+    
+</div>
