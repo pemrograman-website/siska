d7202b2d6130d181b0ea7e097dbe286ee3661ecc
Yusuf Yudhistira <yudhis96@yahoo.com>
2021-10-18 13:51:28 +0700

    CRUD tabel Fakultas

---
+++ backend/views/fakultas/_form.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\helpers\Html;
+use yii\bootstrap4\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\Fakultas */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="fakultas-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'kode')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'nama')->textInput(['maxlength' => true]) ?>
+
+
+    <?php if (!Yii::$app->request->isAjax) { ?>
+        <div class="form-group">
+            <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        </div>
+    <?php } ?>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
